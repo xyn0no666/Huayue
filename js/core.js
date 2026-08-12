7d3eb769dc8c6db6f7a979dad87ff06ddf0862bb
Developer <dev@huayueyuanlin.com>
2026-08-12 10:50:40 +0800

    修复Tawk.to: Tawk_API改为全局变量，修复聊天组件无法加载

--- js/core.js
+++ js/core.js
@@ -382,7 +382,7 @@
 
   /* === Tawk.to Live Chat === */
   function initTawkTo(){
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    window.Tawk_API=window.Tawk_API||{}; window.Tawk_LoadStart=new Date();
     (function(){
     var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
     s1.async=true;
